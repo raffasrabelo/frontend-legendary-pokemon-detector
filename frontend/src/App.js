32d0592de203c7c1268c90127bd167322198d0ba
Rafael Rabelo <raffasrabelo@hotmail.com>
2025-12-04 16:51:42 -0300

    Adicionando frontend corretamente

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [stats, setStats] = useState({
+    hp: 0, attack: 0, defense: 0,
+    sp_atk: 0, sp_def: 0, speed: 0
+  });
+  
+  const [resultado, setResultado] = useState(null);
+
+  const handleChange = (e) => {
+    setStats({ ...stats, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/predict', stats);
+      setResultado(response.data);
+    } catch (error) {
+      console.error("Erro", error);
+    }
+  };
+
+  return (
+    <div className="pokedex-container">
+      <div className="pokedex-header">
+        <h1>Detector de Lendários</h1>
+      </div>
+      
+      <form onSubmit={handleSubmit} className="stats-grid">
+        {Object.keys(stats).map((key) => (
+          <div key={key} className="stat-input">
+            <label>{key.toUpperCase()}</label>
+            <input 
+              type="number" 
+              name={key} 
+              value={stats[key]} 
+              onChange={handleChange} 
+              required 
+            />
+          </div>
+        ))}
+        <button type="submit" className="scan-btn">ANALISAR POKÉMON</button>
+      </form>
+
+      {resultado && (
+        <div className={`result-box ${resultado.is_legendary ? 'legendary' : 'common'}`}>
+          <h2>Resultado: {resultado.resultado}</h2>
+          <p>{resultado.is_legendary ? 'Este Pokémon é extremamente raro e poderoso!' : 'Apenas um Pokémon selvagem comum.'}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
